c105ef8175469e324a3dc86031d3eadc68d5affe
Carlos Abreu <35485515+cabreu145@users.noreply.github.com>
2021-06-27 16:16:51 -0500

    Viewset por cat y tipomaterial

--- listelement/viewsets.py
+++ listelement/viewsets.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from .models import Pieza, Estado_pieza, Tipo_material, Categoria
 from .serializer import PiezaSerializer, Estado_piezaSerializer, Tipo_materialSerializer, CategoriaSerializer
@@ -16,6 +18,18 @@ class Tipo_materialViewSet (viewsets.ModelViewSet):
     queryset = Tipo_material.objects.all()
     serializer_class = Tipo_materialSerializer
 
+    @action(detail=True, methods=['get'])
+    def piezas(self, request, pk=None):
+        queryset = Pieza.objects.filter(tipo_material_id=pk)
+        serializer = PiezaSerializer(queryset, many=True)
+        return Response(serializer.data) 
+
 class CategoriaViewSet(viewsets.ModelViewSet):
     queryset = Categoria.objects.all()
-    serializer_class = CategoriaSerializer 
+    serializer_class = CategoriaSerializer
+
+    @action(detail=True, methods=['get'])
+    def piezas(self, request, pk=None):
+        queryset = Pieza.objects.filter(categoria_id=pk)
+        serializer = PiezaSerializer(queryset, many=True)
+        return Response(serializer.data) 
